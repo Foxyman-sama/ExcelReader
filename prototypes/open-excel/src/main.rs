a28efd2dbcadfe6ae923b9526f536d8683b1833e
Dmytro Latokho <dimalatoho1448@gmail.com>
2025-12-22 07:43:40 +0200

    realize finding first worksheet

--- prototypes/open-excel/src/main.rs
+++ prototypes/open-excel/src/main.rs
@@ -3,9 +3,9 @@ use calamine::{Reader, Xlsx, open_workbook};
 fn main() -> Result<(), Box<dyn std::error::Error>> {
   let path = format!("{}\\tests\\test.xlsx", env!("CARGO_MANIFEST_DIR"));
   let mut excel: Xlsx<_> = open_workbook(path)?;
-  if let Ok(r) = excel.worksheet_range("Подія") {
+  if let Some(Ok(r)) = excel.worksheet_range_at(0) {
     for row in r.rows() {
-      println!("row={:?}, row[0]={:?}", row, row[0]);
+      println!("{:?}", row);
     }
   }
 
