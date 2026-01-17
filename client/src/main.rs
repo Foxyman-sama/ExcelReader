6a87084cf8772a44d90f11948a8e9a789ac73175
Dmytro Latokho <dimalatoho1448@gmail.com>
2026-01-17 22:50:25 +0200

    realize basic reading and output

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,5 +1,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use calamine::{Reader, Xlsx, open_workbook};
 use eframe::egui;
 use egui_extras::{Column, TableBuilder};
 
@@ -22,8 +23,17 @@ struct ExcelReader {
 
 impl Default for ExcelReader {
   fn default() -> Self {
-    let data = Vec::new();
-    // read test excel
+    let mut data = Vec::new();
+    let path = format!("{}\\tests\\test.xlsx", env!("CARGO_MANIFEST_DIR"));
+    let mut excel: Xlsx<_> = open_workbook(path).unwrap();
+    if let Some(Ok(r)) = excel.worksheet_range_at(0) {
+      for row in r.rows() {
+        let row_strings: Vec<String> =
+          row.iter().map(|cell| cell.to_string()).collect();
+
+        data.push(row_strings);
+      }
+    }
 
     Self { data }
   }
@@ -31,6 +41,35 @@ impl Default for ExcelReader {
 
 impl eframe::App for ExcelReader {
   fn update(&mut self, ctx: &eframe::egui::Context, frame: &mut eframe::Frame) {
-    egui::CentralPanel::default().show(ctx, |ui| {});
+    egui::CentralPanel::default().show(ctx, |ui| {
+      ui.heading("ExcelReader");
+
+      TableBuilder::new(ui)
+        .striped(true)
+        .resizable(true)
+        .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
+        .columns(Column::initial(100.0).resizable(true), self.data.len())
+        .header(20.0, |mut header| {
+          let headers = &self.data[0];
+          for col in headers {
+            header.col(|ui| {
+              ui.strong(col);
+            });
+          }
+        })
+        .body(|body| {
+          let row_height = 10.0;
+          let num_rows = self.data.len() - 1;
+          body.rows(row_height, num_rows, |mut row| {
+            let row_index = row.index();
+            let row_data = &self.data[row_index + 1];
+            for cell_text in row_data {
+              row.col(|ui| {
+                ui.label(cell_text);
+              });
+            }
+          });
+        });
+    });
   }
 }
